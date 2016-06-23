58ffc8170bbcad802e51f90c78b1d8d7ef18af12
laras126 <laras126@gmail.com>
2016-06-23 11:49:54 -0400

    add gulp and minified files

---
+++ gulpfile.js
@@ -0,0 +1,67 @@
+
+'use strict';
+
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    svgmin = require('gulp-svgmin'),
+    svgstore = require('gulp-svgstore'),
+    path = require('path'),
+    autoprefix = require('gulp-autoprefixer'),
+    cssnano = require('gulp-cssnano'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    rename = require('gulp-rename');
+
+// Put JS files into array
+var jsFileList = [
+  'assets/js/vendor/*.js',
+  'assets/js/main.js'
+];
+
+gulp.task('sass', function() {
+  return gulp.src('assets/scss/main.scss')
+    .pipe(sass())
+    .pipe(autoprefix({
+        browsers: 'last 5 versions'
+    }))
+    // .pipe(gulp.dest('assets/css'))
+});
+
+gulp.task('js', function() {
+  return gulp.src(jsFileList)
+    .pipe(concat('scripts.js'))
+    .pipe(gulp.dest('assets/js/build'))
+});
+
+gulp.task('min-js', function() {
+  return gulp.src('assets/js/build/scripts.js')
+    .pipe(uglify())
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('assets/js/build'))
+});
+
+gulp.task('watch', function() {
+  gulp.watch('assets/scss/**/*.scss', ['sass']);
+});
+
+gulp.task('svgs', function () {
+  return gulp
+    .src('assets/img/svg/*.svg')
+    .pipe(rename({prefix: 'shape-'}))
+    .pipe(svgmin(function (file) {
+        var prefix = path.basename(file.relative, path.extname(file.relative));
+        return {
+          plugins: [{
+            cleanupIDs: {
+              prefix: prefix + '-',
+              minify: true
+            }
+          }]
+        }
+    }))
+    .pipe(svgstore())
+    .pipe(rename('svg-defs.svg'))
+    .pipe(gulp.dest('views/utility'));
+});
+
+// sass --watch input:output
